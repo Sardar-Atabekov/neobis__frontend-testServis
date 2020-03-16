0fdf20396072e4a01525d55d946b23a67ecc5daa
Sardar-Atabekov <43814348+Sardar-Atabekov@users.noreply.github.com>
2020-03-16 21:21:39 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,40 @@
-let wrapper = document.querySelector(".wrapper");
+Aman, [16.03.20 18:00]
+let wrapper = document.querySelector('.wrapper');
 
-Song = new Audio("00183.mp3");
-wrapper.addEventListener("mouseout", () => {
-  Song.play();
-  setTimeout(() => {
-    alert("Мы же тебя предупреждали!! -10 баллов!");
-  }, 400);
-  setTimeout(() => {
-    Song.pause();
-  }, 3000);
+
+Song = new Audio('00183.mp3');
+wrapper.addEventListener('mouseout', ()=>{
+  Song.play()
+  setTimeout(()=>{
+    alert('Дибил мы же предупреждали')
+  },400)
+  setTimeout(()=>{
+    Song.pause()
+  },3000)
+})
+
+Aman, [16.03.20 18:27]
+let wrapper = document.querySelector('.wrapper');
+
+
+let play = () =>{
+  Song.play()
+  setTimeout(()=>{
+    alert('мы же предупреждали')
+  },400)
+  setTimeout(()=>{
+    Song.pause()
+  },3000)
+} 
+Song = new Audio('00183.mp3');
+wrapper.addEventListener('mouseout', ()=>{
+  play()
+})
+
+window.addEventListener('keydown', function(e) {
+  if (e.keyCode === 17) {
+   play()
+  }else if (e.keyCode === 18){
+    play()
+  }
 });
