8aa6e678252a26149e71dac49339bd3ea3609e73
Sardar Atavekov <sardarbekatabekov@gmail.com>
2020-03-16 21:36:27 +0600

    update js check

--- main.js
+++ main.js
@@ -1,40 +1,23 @@
+let wrapper = document.querySelector(".wrapper");
 
-let wrapper = document.querySelector('.wrapper');
+let play = () => {
+  Song.play();
+  setTimeout(() => {
+    alert("Мы тебя предупреждали!! Минус 10 баллов");
+  }, 400);
+  setTimeout(() => {
+    Song.pause();
+  }, 3000);
+};
+Song = new Audio("00183.mp3");
+wrapper.addEventListener("mouseout", () => {
+  play();
+});
 
-
-Song = new Audio('00183.mp3');
-wrapper.addEventListener('mouseout', ()=>{
-  Song.play()
-  setTimeout(()=>{
-    alert('Дибил мы же предупреждали')
-  },400)
-  setTimeout(()=>{
-    Song.pause()
-  },3000)
-})
-
-Aman, [16.03.20 18:27]
-let wrapper = document.querySelector('.wrapper');
-
-
-let play = () =>{
-  Song.play()
-  setTimeout(()=>{
-    alert('мы же предупреждали')
-  },400)
-  setTimeout(()=>{
-    Song.pause()
-  },3000)
-} 
-Song = new Audio('00183.mp3');
-wrapper.addEventListener('mouseout', ()=>{
-  play()
-})
-
-window.addEventListener('keydown', function(e) {
+window.addEventListener("keydown", function(e) {
   if (e.keyCode === 17) {
-   play()
-  }else if (e.keyCode === 18){
-    play()
+    play();
+  } else if (e.keyCode === 18) {
+    play();
   }
 });
